08318c9ec5616278a6d64f526c5a770fec4cd1b6
Mushtaq Ahmed <mushtaqjugnu04@gmail.com>
2025-08-06 08:50:59 +0500

    python files

---
+++ condition.py
@@ -0,0 +1,19 @@
+# age = int(input("Enter a number"))
+
+# # print("elder") if age == 18 else print("older")
+
+# age = ("Yes","No") [age ==18]
+
+# print (age)
+
+a = 30
+b = 50
+
+print (not True)
+print (not( a > b ))
+
+var1 = True
+var2 = False
+
+print (not True)
+print ("print", var1 or var2)
